ad6bc0b1fbba55cf73b11eb3f298cc272e66eb42
jammie-commits <jay.mbugua.ph@gmail.com>
2024-05-02 09:53:32 +0300

    updated files

--- bank-of-flatiron/src/components/table/Transactiontable.jsx
+++ bank-of-flatiron/src/components/table/Transactiontable.jsx
@@ -1,45 +1,22 @@
-import React from "react";
-import styles from "./table.module.css";
-
-// directly destructure transactions
-const Table = ({ transactions, filter }) => {
-  // !show records based on category
-  if (filter) {
-    transactions = transactions.filter((transaction) => {
-      return transaction.category.toLowerCase() === filter.toLowerCase();
-    });
-  }
+function TransactionList() {
   return (
-    <table className={styles.table}>
+    <table>
       <thead>
         <tr>
-          <th>ID</th>
           <th>Date</th>
           <th>Description</th>
-          <th>Category</th>
           <th>Amount</th>
         </tr>
       </thead>
       <tbody>
-        {/* conditionally render transactions/short-circuit */}
-
-        {transactions &&
-          transactions.map((transaction) => {
-            const { id, date, description, category, amount } = transaction;
-            return (
-              // !lists and keys
-              <tr key={id}>
-                <td>{id}</td>
-                <td>{date}</td>
-                <td>{description}</td>
-                <td>{category}</td>
-                <td>{amount}</td>
-              </tr>
-            );
-          })}
+        {transactions.map((transaction) => (
+          <tr key={transaction.id}>
+            <td>{transaction.date}</td>
+            <td>{transaction.description}</td>
+            <td>{transaction.amount.toFixed(2)}</td>  // Format amount to 2 decimal places
+          </tr>
+        ))}
       </tbody>
     </table>
   );
-};
-
-export default Table;
+}
